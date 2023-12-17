395c560d9d7c6d96880afaf62bd00d5d72c7d4df
YOUR <r.murara@alustudent.com>
2023-12-17 11:53:55 -0800

    all tasks done

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,16 @@
-# make changes to config file using Puppet
-
+# Configures ssh for dev
 include stdlib
 
-file_line { 'Refuse to authenticate using a password':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
+file_line { 'no_password_auth':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
 }
 
-file_line { 'Use private key':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school'
+file_line { 'identity_file':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+  replace => true,
 }
